a7763abcf88114c9f3fd29606e3690fa8be202a5
Alberto Diaz lopez <adiaz-lo@c1r17s2.42madrid.com>
2023-04-04 16:29:09 +0200

    Fixed Norminette errors with ft_strnstr & started ft_atoi

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,13 +6,16 @@
 /*   By: adiaz-lo <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/31 12:28:24 by adiaz-lo          #+#    #+#             */
-/*   Updated: 2023/03/31 13:08:52 by adiaz-lo         ###   ########.fr       */
+/*   Updated: 2023/04/04 15:33:03 by adiaz-lo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 int	ft_isspace (int c)
 {
-	
+	if (c == '\t' || c == '\n' || c == '\v' || c == '\f' || c == '\r' || c == ' '
+			return (1);
+	else
+		return (0);
 }
 
 int ft_atoi (const char *string)
--- ft_strnstr.c
+++ ft_strnstr.c
@@ -1,32 +1,34 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strnstr_.c                                      :+:      :+:    :+:   */
+/*   ft_strnstr.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: adiaz-lo <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/29 16:17:54 by adiaz-lo          #+#    #+#             */
-/*   Updated: 2023/03/29 17:31:17 by adiaz-lo         ###   ########.fr       */
+/*   Updated: 2023/04/04 15:37:42 by adiaz-lo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-char	*ft_strnstr_(const char* haystack, const char* needle, size_t len){
+char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
+{
 	int	i;
 	int	j;
-	
+
 	if (!haystack)
 		return (0);
-	if (!needle){
+	if (!needle)
 		return ((char *)haystack);
-	}
 	i = 0;
-	while (haystack[i] && i < len){
+	while (haystack[i] && i < len)
+	{
 		j = 0;
-		while (needle[j] == haystack[i + j]){
+		while (needle[j] == haystack[i + j])
+		{
 			if (i + j >= len)
 				return (0);
 			if (needle[++j] == 0)
-				return (char *)(haystack + i);
+				return ((char *)(haystack + i));
 		}
 		i++;
 	}

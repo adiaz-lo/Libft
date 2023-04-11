b4d76850ecff96e5b773bed8ea65f433fc230c2c
Alberto Diaz lopez <adiaz-lo@c2r19s1.42madrid.com>
2023-04-11 16:01:05 +0200

    Advanced ft_substr.c

--- ft_substr.c
+++ ft_substr.c
@@ -6,16 +6,20 @@
 /*   By: adiaz-lo <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/11 09:25:20 by adiaz-lo          #+#    #+#             */
-/*   Updated: 2023/04/11 12:18:08 by adiaz-lo         ###   ########.fr       */
+/*   Updated: 2023/04/11 16:00:19 by adiaz-lo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 char *ft_substr(char const *s, unsigned int start, size_t len)
 {
 	const size_t	sl = ft_strlen(s);
+	const size_t	strlfmsta = sl - start; //str length from start
 	char			*str;
 
-	if (len < sl)
+	if (!(strlfmsta))
+		return ();
+	if (sl - start < len)
+		malloc(strllfmsta + 1)
 		
 	return (str);
 }

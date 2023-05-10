56892d074d9eaeb0489beb60e00f6897f8142c0e
Alberto Diaz lopez <adiaz-lo@c1r7s3.42madrid.com>
2023-05-10 17:45:29 +0200

    Advanced ft_putnbr_fd

--- ft_putnbr_fd.c
+++ ft_putnbr_fd.c
@@ -1,6 +1,19 @@
 #include "libft.h"
 
-void ft_putnbr_fd(int n, int fd)
+void	ft_putnbr_fd(int n, int fd)
 {
-	
+	int	ntp;
+
+	if (n < 0)
+	{
+		ntp = n % 10;
+		ft_putchar_fd(ntp + '', fd);
+		ft_putnbr_fd(n / 10, fd);
+	}
+}
+
+int	main()
+{
+	ft_putnbr_fd(42, 1);
+	return(0);
 }
---
+++ main_ft_putnbr_fd.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+void    ft_putnbr_fd(int n, int fd);
+
+int main(int argc, char *argv[])
+{
+	
+}

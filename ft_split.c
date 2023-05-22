f20269807896f3715a7f8a5389d8ded9f81b6875
Alberto Diaz lopez <adiaz-lo@c1r7s3.42madrid.com>
2023-05-22 18:02:31 +0200

    Deleted old count words function in ft_split

--- ft_split.c
+++ ft_split.c
@@ -12,24 +12,6 @@
 
 #include "libft.h"
 
-/*int	ft_count_words(char const *s, char c)
-{
-	int			tnum;
-
-	tnum = 0;
-	if (!s || *s == '\0')
-		return (0);
-	while (*s != '\0')
-	{
-		if (*s == c && *(s - 1) != c)
-			tnum++;
-		s++;
-	}
-	if (*(s - 1) != c)
-		tnum++;
-	return (tnum);
-}*/
-
 int	ft_count_words(char const *s, char c)
 {
 	unsigned int	i;

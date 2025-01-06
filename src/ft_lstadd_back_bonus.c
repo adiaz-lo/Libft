7cf29df795efdfb216cfb94e25f0b589971761fc
Alberto Diaz lopez <adiaz-lo@c2r12s2.42madrid.com>
2025-01-06 18:03:32 +0100

    Fixed lists prototypes parameter name to be Linux compliant

--- src/ft_lstadd_back_bonus.c
+++ src/ft_lstadd_back_bonus.c
@@ -12,15 +12,15 @@
 
 #include "../includes/libft.h"
 
-void	ft_lstadd_back(t_list **lst, t_list *new)
+void	ft_lstadd_back(t_list **lst, t_list *new_node)
 {
 	t_list	*l_last;
 
 	if (!*lst)
-		*lst = new;
+		*lst = new_node;
 	else
 	{
 		l_last = ft_lstlast(*lst);
-		l_last->next = new;
+		l_last->next = new_node;
 	}
 }
